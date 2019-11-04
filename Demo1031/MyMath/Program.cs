399cd6888530ce6a272176d3c1ac6ec3d1257dde
a.khan <a.khan4@students.clark.edu>
2019-11-03 22:33:05 -0800

    demo 1031

---
+++ Demo1031/MyMath/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("101 + 99 = {0}", MyMath1.Add(101, 99));
+            // this wil result in 
+            Console.WriteLine("101 + 201 = {0}", MyMath1.Add(101, 201));
+            // byte is 255 bits, insignificant bits will be lost 
+        }
+    }
+}
---
+++ Demo1031/MyMath1.UnitTests/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("MyMath1.UnitTests")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("MyMath1.UnitTests")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b045453f-ff4b-4d8e-83f1-81034c61c6e5")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
